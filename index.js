b9f79e8246838653e1d8de7535e9cdce2ba786b5
Jessica Gladstone <jcg321@comcast.net>
2018-09-09 17:19:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,9 @@ function prependKitten(name) {
 function removeLastKitten() {
   return kittens.slice(0, kittens.length -1);
 }
-
+function removeFirstKitten() {
+  return kittens.slice(0);
+}
 
 
 
